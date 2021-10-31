113ca964a7f7058788395e7dfe2622d9f2bf2246
abrorbatirovic <ashurmatovabror1107@gmail.com>
2021-10-31 17:09:31 +0500

    first commit

--- src/page/Enter.js
+++
@@ -1,9 +0,0 @@
-import {Link} from "react-router-dom";
-
-function Enter() {
-    return (
-        <Link to={'/sign-up'}><button className={'btn-enter'}>Enter</button></Link>
-    )
-}
-
-export default Enter
